d301fa3cc377bed52c9c005e58717b03c23db475
capocattiveria <1dnicofiorini.nf@gmail.com>
2025-05-02 15:21:00 +0200

    Cleaning some annoying comments and refactor directory structure

--- lua/plugins/keymap.lua
+++ lua/plugins/keymap.lua
@@ -5,10 +5,10 @@ function keymaps.setup()
   vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
 
   -- Modalità normale: esempio di mappa per salvare velocemente
-  vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true, silent = false })
+  vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = true, silent = false, desc = 'Save buffer' })
 
   -- Modalità normale: esempio di mappa per chiudere la finestra
-  vim.keymap.set('n', '<leader>q', ':q<CR>', { noremap = true, silent = false })
+  vim.keymap.set('n', '<leader>q', ':q<CR>', { noremap = true, silent = false, desc = 'Quit' })
 end
 
 return keymaps
